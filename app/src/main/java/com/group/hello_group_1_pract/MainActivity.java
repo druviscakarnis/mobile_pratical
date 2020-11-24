c5d4c6867995241939b9bb4b81f5b26077e4a021
druviscakarnis <druvis.cakarnis@gmail.com>
2020-11-24 12:29:03 +0200

    Druvis Čakārnis
    1. individuālais praktiskais

---
+++ app/src/main/java/com/group/hello_group_1_pract/MainActivity.java
@@ -0,0 +1,14 @@
+package com.group.hello_group_1_pract;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
